b5b02753a0dfcb98fe98c191d30dac4ce23c590a
Frem! <emilianoalbamonte@gmail.com>
2023-01-05 13:27:12 +0100

    Grid dispuesto

--- prac1-font-end/src/App.jsx
+++ prac1-font-end/src/App.jsx
@@ -7,9 +7,14 @@ function App() {
   return (
     <div className="App">
       <div className="parent">
-        <div className="div1">div1</div>
-        <div className="div2">div2</div>
-        <div className="div3">div3</div>
+        <div className="div1"><button className="colButton">Listar</button><button className="colButton">Monitorizar</button></div>
+        <div className="div2"><button className="colButton">Listar</button><button className="colButton">Monitorizar</button></div>
+        <div className="div3">
+          <form action="action_page.php">
+          <input type="text" placeholder="Search.." name="search" />
+          <button type="submit">Agregar<i className="fa fa-search"></i></button>
+          </form>
+        </div>
         <div className="div4">
           <textarea readOnly={true} name="textarea" rows="10" cols="50" value="">
             Write something here
